2fc9046cf8b61d30942f06a02cd3d22c8b98cb8e
Hemmyrald <55050684+Hemmyrald@users.noreply.github.com>
2023-09-18 01:26:58 +0100

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset, DataLoader
 from preprocess import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-with open('/home/hemmyrald/anaconda3/envs/chatbot/fyp/intents.json', 'r') as f:
+with open('intents.json', 'r') as f:
     intents = json.load(f)
 
 all_words = []
